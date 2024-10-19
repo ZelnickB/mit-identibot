ZelnickB/mit-identibot

commit 36b37cf99e0144777bbac06df892e60c287bca38

Improved account identification functionality
This commit allows the `/whois` and “Get Profile from Linked Kerberos” commands to provide more information to server administrators by harnessing the MIT Developer Connection People API. The information is presented in the form of an embed, which is easier to tread than plain text and is able to effectively organize the additional information.

discordBot/commands/ChatInput/logic/whois.js
@@ -1,13 +1,22 @@
-import { profileText } from '../../common/whois.js'
+import { getUserInfo } from '../../common/whois/retrievers.js'
 import { authorizeAndReply } from '../../serverAuthorization.js'
+import { whoisResult } from '../../common/whois/embedBuilders.js'
 
 export default async function (interaction) {
   if (!await authorizeAndReply(interaction)) return
-  interaction.reply({
-    content: await profileText(interaction.options.get('user').user),
-    allowedMentions: {
-      parse: []
-    },
-    ephemeral: interaction.options.get('onlyme') !== null && interaction.options.get('onlyme').value
-  })
+  try {
+    const embed = await (whoisResult(interaction.options.get('user').user.id, getUserInfo(interaction.options.get('user').user)))
+    interaction.reply({
+      embeds: [embed],
+      allowedMentions: {
+        parse: []
+      },
+      ephemeral: interaction.options.get('onlyme') !== null && interaction.options.get('onlyme').value
+    })
+  } catch {
+    interaction.reply({
+      content: '**Error:** IdentiBot encountered a problem while fetching the information for this user.',
+      ephemeral: true
+    })
+  }
 }

discordBot/commands/User/logic/Get Profile from Linked Kerberos.js
@@ -1,13 +1,22 @@
-import { profileText } from '../../common/whois.js'
+import { getUserInfo } from '../../common/whois/retrievers.js'
 import { authorizeAndReply } from '../../serverAuthorization.js'
+import { whoisResult } from '../../common/whois/embedBuilders.js'
 
 export default async function (interaction) {
   if (!await authorizeAndReply(interaction)) return
-  interaction.reply({
-    content: await profileText(interaction.options.get('user').user),
-    allowedMentions: {
-      parse: []
-    },
-    ephemeral: true
-  })
+  try {
+    const embed = await (whoisResult(interaction.options.get('user').user.id, getUserInfo(interaction.options.get('user').user)))
+    interaction.reply({
+      embeds: [embed],
+      allowedMentions: {
+        parse: []
+      },
+      ephemeral: true
+    })
+  } catch {
+    interaction.reply({
+      content: '**Error:** IdentiBot encountered a problem while fetching the information for this user.',
+      ephemeral: true
+    })
+  }
 }

discordBot/commands/common/whois/embedBuilders.js
@@ -1,0 +1,89 @@
+import { EmbedBuilder } from 'discord.js'
+import { config as configReader } from '../../../../lib/preferencesReader.js'
+import { dbClient } from '../../../../lib/mongoClient.js'
+
+const config = await configReader()
+const verificationUserInfoCollection = dbClient.collection('verification.userInfo')
+
+export async function whoisResult (discordID, userInfo) {
+  let affiliationColorCode
+  const embedBuilder = new EmbedBuilder()
+    .setTitle('Verified User Information')
+    .setDescription(`The Discord user <@${discordID}> has been identified as the following individual.`)
+    .addFields(
+      {
+        name: 'Legal Name',
+        value: `${userInfo.familyName}, ${userInfo.givenName}${userInfo.middleName ? ' ' + userInfo.middleName : ''}`,
+        inline: true
+      },
+      {
+        name: 'Display Name',
+        value: userInfo.displayName,
+        inline: true
+      },
+      {
+        name: 'Email/Kerberos',
+        value: `${userInfo.email} (\`${userInfo.kerberosId}\`)`
+      }
+    )
+  if (userInfo.phoneNumber) {
+    embedBuilder.addFields({
+      name: 'Phone Number',
+      value: userInfo.phoneNumber
+    })
+  }
+  let positionTitle, officeLocation
+  const departmentNames = []
+  if (userInfo.affiliations.length > 0) {
+    embedBuilder.addFields({
+      name: 'Affiliation Type',
+      value: userInfo.affiliations[0].type,
+      inline: true
+    })
+    for (const department of userInfo.affiliations[0].departments) {
+      departmentNames.push(department)
+    }
+    positionTitle = userInfo.affiliations[0].title
+    officeLocation = userInfo.affiliations[0].office
+    switch (userInfo.affiliations[0].type) {
+      case 'faculty':
+        affiliationColorCode = 0x8800FF
+        break
+      case 'staff':
+        affiliationColorCode = 0x00CCFF
+        break
+      case 'affiliate':
+        affiliationColorCode = 0xFFD900
+        break
+      case 'student':
+        affiliationColorCode = 0x88FF00
+        break
+      default:
+        affiliationColorCode = 0xFFFFFF
+        break
+    }
+  } else affiliationColorCode = 0xFFFFFF
+  if (positionTitle) {
+    embedBuilder.addFields({
+      name: 'Title',
+      value: positionTitle,
+      inline: true
+    })
+  }
+  if (departmentNames.length > 0) {
+    embedBuilder.addFields({
+      name: 'Departments',
+      value: departmentNames.join(', '),
+      inline: true
+    })
+  }
+  if (officeLocation) {
+    embedBuilder.addFields({
+      name: 'Office',
+      value: officeLocation,
+      inline: true
+    })
+  }
+  embedBuilder.setColor(affiliationColorCode)
+  return embedBuilder
+}

discordBot/commands/common/whois/retrievers.js
@@ -1,0 +1,29 @@
+import { dbClient } from '../../../../lib/mongoClient.js'
+import { getByKerberos } from '../../../../lib/mitDeveloperConnection/people.js'
+
+const verificationUserInfoCollection = dbClient.collection('verification.userInfo')
+
+export class UnlinkedUserError extends Error {
+  constructor (userId) {
+    super(`User ${userId} has not linked a Discord account.`)
+    this.name = this.constructor.name
+  }
+}
+
+export async function getKerberos (user) {
+  return verificationUserInfoCollection.findOne(
+    {
+      'discord.id': user.id
+    }
+  ).then((doc) => {
+    if (doc === null) {
+      throw UnlinkedUserError(user.id)
+    } else {
+      return doc.petrock.email
+    }
+  })
+}
+
+export async function getUserInfo (user) {
+  return getByKerberos(await getKerberos(user.id))
+}

discordBot/commands/common/whois.js
@@ -1,17 +1,0 @@
-import { dbClient } from '../../../lib/mongoClient.js'
-
-const verificationUserInfoCollection = dbClient.collection('verification.userInfo')
-
-export async function profileText (user) {
-  return verificationUserInfoCollection.findOne(
-    {
-      'discord.id': user.id
-    }
-  ).then((doc) => {
-    if (doc === null) {
-      return '**Error:** The specified Discord user is not linked to a Kerberos account.'
-    } else {
-      return `<@${user.id}> is verified to control the Kerberos account with the following profile information:\n**${doc.petrock.name}**\n**Affiliation:** ${doc.petrock.affiliation}\n**Email:** \`${doc.petrock.email}\``
-    }
-  })
-}

lib/mitDeveloperConnection/people.js
@@ -1,0 +1,26 @@
+import * as preferencesReader from '../preferencesReader.js'
+
+const config = await preferencesReader.config()
+const secrets = await preferencesReader.secrets()
+
+export class UserNotFoundError extends Error {
+  constructor (kerb) {
+    super(`Information about user with Kerberos identity ${kerb} was not found.`)
+    this.name = this.constructor.name
+  }
+}
+
+export async function getByKerberos (kerb) {
+  return fetch(
+    `https://mit-people-v3.cloudhub.io/people/v3/people/${kerb}`,
+    {
+      headers: {
+        client_id: config.mitDeveloperConnection.clientID,
+        client_secret: secrets.mitDeveloperConnectionClientSecret
+      }
+    }
+  ).then(res => {
+    if (res.status === 404) return Promise.reject(new UserNotFoundError(kerb))
+    return res.json()
+  }).then(responseJSON => responseJSON.item)
+}

samples/config.yml
@@ -8,6 +8,8 @@ mongodb:
   dbName: "identibot"
 petrock:
   clientID: "ABCDEFGHIJK"
+mitDeveloperConnection:
+  clientID: "0123456789"
 discord:
   clientID: "12345678901234567890"
 commandSettings:

samples/secrets.yml
@@ -2,6 +2,7 @@ symmetricCipherKey: 'ABCDEFG12345'
 oauthClientSecrets:
   petrock: '0123456789abcdef'
   discord: 'AbCdEfGhIj'
+mitDeveloperConnectionClientSecret: '0123456789ABCDEF'
 discordBotToken: 'ABCDEFGHIJK'
 mongodb:
   connectionPassword: "password" # Optional; omit if username/password authentication is not used.

commit a6872496b1ac384ffa165873a18f63a78f5a69ed

Bugfixes from 36b37cf9
This commit corrects several bugs that were introduced in commit 36b37cf9.

discordBot/commands/ChatInput/logic/whois.js
@@ -5,7 +5,7 @@ import { whoisResult } from '../../common/whois/embedBuilders.js'
 export default async function (interaction) {
   if (!await authorizeAndReply(interaction)) return
   try {
-    const embed = await (whoisResult(interaction.options.get('user').user.id, getUserInfo(interaction.options.get('user').user)))
+    const embed = await whoisResult(interaction.options.get('user').user.id, await getUserInfo(interaction.options.get('user').user))
     interaction.reply({
       embeds: [embed],
       allowedMentions: {

discordBot/commands/User/logic/Get Profile from Linked Kerberos.js
@@ -5,7 +5,7 @@ import { whoisResult } from '../../common/whois/embedBuilders.js'
 export default async function (interaction) {
   if (!await authorizeAndReply(interaction)) return
   try {
-    const embed = await (whoisResult(interaction.options.get('user').user.id, getUserInfo(interaction.options.get('user').user)))
+    const embed = await whoisResult(interaction.options.get('user').user.id, await getUserInfo(interaction.options.get('user').user))
     interaction.reply({
       embeds: [embed],
       allowedMentions: {

discordBot/commands/common/whois/embedBuilders.js
@@ -41,7 +41,7 @@ export async function whoisResult (discordID, userInfo) {
       inline: true
     })
     for (const department of userInfo.affiliations[0].departments) {
-      departmentNames.push(department)
+      departmentNames.push(department.name)
     }
     positionTitle = userInfo.affiliations[0].title
     officeLocation = userInfo.affiliations[0].office

discordBot/commands/common/whois/retrievers.js
@@ -17,13 +17,13 @@ export async function getKerberos (user) {
     }
   ).then((doc) => {
     if (doc === null) {
-      throw UnlinkedUserError(user.id)
+      return Promise.reject(new UnlinkedUserError(user.id))
     } else {
-      return doc.petrock.email
+      return doc.petrock.email.replace(/@mit\.edu/gi, '')
     }
   })
 }
 
 export async function getUserInfo (user) {
-  return getByKerberos(await getKerberos(user.id))
+  return getByKerberos(await getKerberos(user))
 }