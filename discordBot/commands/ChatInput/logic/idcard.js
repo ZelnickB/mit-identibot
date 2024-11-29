6f0efd769e8ef153afa022f714b30542af70aaf6
Benjamin Zelnick <34197604+ZelnickB@users.noreply.github.com>
2024-11-29 00:59:29 -0500

    Added `/idcard` command
    This commit adds the `/idcard` command, which allows a Discord user to display information about their linked Kerberos identity in a channel. The user can also choose to display their MIT ID card photo (if desired).

---
+++ discordBot/commands/ChatInput/logic/idcard.js
@@ -0,0 +1,64 @@
+import { getUserInfo, UnlinkedUserError } from '../../common/whois/retrievers.js'
+import { AttachmentBuilder, EmbedBuilder } from 'discord.js'
+
+export default async function (interaction) {
+  const showIdPhoto = interaction.options.getBoolean('withpicture') !== false
+  const ephemeral = interaction.options.getBoolean('preview') === true
+  interaction.deferReply({ ephemeral })
+  try {
+    const userInfo = await getUserInfo(interaction.user, showIdPhoto)
+    console.log(userInfo)
+    const embedBuilder = new EmbedBuilder()
+      .setAuthor({
+        name: 'Massachusetts Institute of Technology'
+      })
+      .setColor(0x750014)
+      .setTitle('Discord ID Card')
+      .addFields([
+        {
+          name: 'Discord User',
+          value: `<@${interaction.user.id}> (\`${interaction.user.id}\`)`
+        },
+        {
+          name: 'Name',
+          value: userInfo.userInfo.displayName,
+          inline: true
+        }
+      ])
+      .setTimestamp(new Date())
+      .setFooter({
+        text: 'Not valid for official identification purposes. Sent upon request.'
+      })
+    if (userInfo.userInfo.affiliations.length > 0) {
+      const affiliationType = userInfo.userInfo.affiliations[0].type
+      embedBuilder.addFields({
+        name: 'Affiliation',
+        value: affiliationType.charAt(0).toUpperCase() + affiliationType.substring(1),
+        inline: true
+      })
+    }
+    embedBuilder.addFields({
+      name: 'Email/Kerberos',
+      value: `${userInfo.userInfo.email.replaceAll('_', '\\_')} (\`${userInfo.userInfo.kerberosId}\`)`,
+      inline: true
+    })
+    const files = []
+    if (userInfo.image !== undefined) {
+      files.push(new AttachmentBuilder(userInfo.image, { name: 'user.jpeg' }))
+      embedBuilder.setThumbnail('attachment://user.jpeg')
+    }
+    return await interaction.editReply({
+      embeds: [embedBuilder],
+      files
+    })
+  } catch (e) {
+    if (e instanceof UnlinkedUserError) {
+      return await interaction.editReply({
+        content: `The user <@${interaction.user.id}> does not have a linked Kerberos identity.`,
+        allowedMentions: {
+          parse: []
+        }
+      })
+    }
+  }
+}
