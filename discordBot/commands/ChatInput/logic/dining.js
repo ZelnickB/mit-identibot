cf1d2dd6255fd4364169a6a6efc2bba3fd6dbfc6
Benjamin Zelnick <34197604+ZelnickB@users.noreply.github.com>
2025-01-17 19:06:38 -0500

    Added `/dining menus` command
    This commit adds the `/dining menus` command (a subcommand of `/dining`), which allows users to see menus for MIT dining venues.
    Daily dining information is cached if the configuration option is enabled, and MongoDB should be set to delete them after 24 hours via its TTL function.

---
+++ discordBot/commands/ChatInput/logic/dining.js
@@ -0,0 +1,68 @@
+import _ from 'lodash'
+import { DateTime } from 'luxon'
+import { EmbedBuilder } from 'discord.js'
+import * as dining from '../../../../lib/publicAPIs/dining.js'
+
+export default async function (interaction) {
+  if (interaction.options.getSubcommandGroup() === null) {
+    switch (interaction.options.getSubcommand()) {
+      case 'menus': {
+        const venue = interaction.options.getString('venue')
+        const meal = interaction.options.getString('meal')
+        const [, mealInfo] = await Promise.all([interaction.deferReply(), dining.getMealInfo(venue, meal)])
+        if (mealInfo === undefined) {
+          return interaction.editReply({
+            content: `**Error:** ${venue} is not open for ${meal.toLowerCase()} today, or the menu is not available.`
+          })
+        }
+        const menu = dining.parseMenu(mealInfo)
+        let embedColor = 0x808080
+        switch (meal) {
+          case 'Breakfast': {
+            embedColor = 0xff8000
+            break
+          }
+          case 'Brunch': {
+            embedColor = 0x00ff60
+            break
+          }
+          case 'Lunch': {
+            embedColor = 0x0080ff
+            break
+          }
+          case 'Dinner': {
+            embedColor = 0xc000ff
+            break
+          }
+          case 'Late Night': {
+            embedColor = 0x001060
+            break
+          }
+        }
+        const embed = new EmbedBuilder()
+          .setTitle(`${dining.getVenueFullName(venue)} ${meal} Menu`)
+          .setAuthor({
+            name: 'Massachusetts Institute of Technology Dining'
+          })
+          .setDescription(`${meal} is being served at ${venue} from ${mealInfo.start_time} to ${mealInfo.end_time} today.`)
+          .setFooter({
+            text: `Menu for ${meal} at ${venue} Dining, ${DateTime.now().setZone('America/New_York').toFormat('ccc d LLLL, yyyy')}.`
+          })
+          .setColor(embedColor)
+        for (const station in menu) {
+          if (['beverages', 'condiments'].includes(station.toLowerCase())) continue
+          embed.addFields({
+            name: _.upperFirst(station),
+            value: '* ' + menu[station].map(x => _.upperFirst(x.name)).join('\n* '),
+            inline: true
+          })
+        }
+        return interaction.editReply({
+          embeds: [embed]
+        })
+      }
+    }
+  } else {
+    return 0
+  }
+}
