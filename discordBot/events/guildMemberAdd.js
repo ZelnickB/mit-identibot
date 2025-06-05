9a3e4b7799282ee6d6f13cbdbb043ab404d2e0b2
Benjamin Zelnick <34197604+ZelnickB@users.noreply.github.com>
2025-06-05 00:20:55 -0400

    Fixed error when unverified user joins server
    This commit fixes an error that was causing the bot to crash whenever an unverified user joined a configured Discord server. It also clarifies the `UnlinkedUserError` error message.

--- discordBot/events/guildMemberAdd.js
+++ discordBot/events/guildMemberAdd.js
@@ -1,8 +1,13 @@
 import { isServerConfigured } from '../../lib/configurationReaders.js'
 import { assignRolesToGuildMember } from '../../lib/guildUserModifier.js'
+import { UnlinkedUserError } from '../../lib/userLinks.js'
 
 export default async function handler (member) {
   if (await isServerConfigured(member.guild.id)) {
-    return assignRolesToGuildMember(member)
+    try {
+      return await assignRolesToGuildMember(member)
+    } catch (e) {
+      if (!(e instanceof UnlinkedUserError)) throw e
+    }
   }
 }
