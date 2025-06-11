37455871ba51ded8ed02afa8ba5bd2aac5cde528
Benjamin Zelnick <34197604+ZelnickB@users.noreply.github.com>
2025-06-11 16:26:20 -0400

    Better handling for users w/directory suppression
    This commit improves handling for users who have directory suppression enabled. The profile information of these users cannot be accessed via the People API. This commit handles errors during automatic role assignment (only the generic role for any affiliation is assigned) and when the /whois and “Get Profile from Linked Kerberos” commands are used.

--- discordBot/commands/common/whois.js
+++ discordBot/commands/common/whois.js
@@ -1,7 +1,8 @@
-import { getUserInfo, UnlinkedUserError } from '../../../lib/userLinks.js'
 import { AttachmentBuilder } from 'discord.js'
 
+import { getUserInfo, UnlinkedUserError } from '../../../lib/userLinks.js'
 import { whoisResult } from '../../embedBuilders.js'
+import { UserNotFoundError } from '../../../lib/mitDeveloperConnection/people.js'
 
 export async function respond (interaction) {
   try {
@@ -22,6 +23,11 @@ export async function respond (interaction) {
         allowedMentions: {
           parse: []
         }
+      })
+    }
+    if (e instanceof UserNotFoundError) {
+      return interaction.editReply({
+        content: '**Error:** Information about the user was unavailable. This error may occur if the user has enabled directory suppression or if they are no longer at MIT.'
       })
     }
     interaction.editReply({
