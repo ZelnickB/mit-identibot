ZelnickB/mit-identibot

commit d0e3180d1900142f65ccae50574071a8ae28fd68

Improved bot command UX for errors
This commit improves the user experience when an error is encountered while running a bot command. All errors are now presented in a standard embed format, which includes a customizable support email address.
This commit also temporarily disables the unstable /dining command.

discordBot/commands/ChatInput/logic/courseinfo.js
@@ -1,5 +1,7 @@
 import * as courses from '../../../../lib/publicAPIs/courses.js'
 import { courseInfo, departmentInfo } from '../../../embedBuilders.js'
+import { EmbeddableError } from '../../../../lib/errorBases.js'
+import { UnknownPrefixError } from '../../../../lib/publicAPIs/courses.js'
 
 export default async function (interaction) {
   const idQuery = interaction.options.get('id').value
@@ -10,23 +12,17 @@ export default async function (interaction) {
         embeds: [courseInfo(courseInformation)]
       })
     } catch (e) {
-      if (e instanceof courses.CourseNotFoundError) {
-        interaction.reply({
-          content: `**Error:** The specified course ID, \`${idQuery}\`, was not found in the course catalog.`,
-          ephemeral: true
-        })
+      if (e instanceof EmbeddableError) {
+        e.replyWithEmbed(interaction)
       }
     }
   } else {
     if (idQuery in courses.departmentInformation) {
-      interaction.reply({
+      return interaction.reply({
         embeds: [departmentInfo(idQuery, courses.departmentInformation[idQuery])]
       })
     } else {
-      interaction.reply({
-        content: `**Error:** The specified prefix, \`${idQuery}\`, is not known.`,
-        ephemeral: true
-      })
+      return UnknownPrefixError(idQuery).replyWithEmbed(interaction)
     }
   }
 }

discordBot/commands/ChatInput/logic/buildinginfo.js
@@ -1,17 +1,12 @@
 import { buildingQuery } from '../../../../lib/publicAPIs/whereis.js'
-import { BadGatewayError } from '../../../../lib/genericErrors.js'
 import { buildingInfo } from '../../../embedBuilders.js'
+import { EmbeddableError } from '../../../../lib/errorBases.js'
 
 export default async function (interaction) {
   const query = interaction.options.get('query').value
   try {
-    const queryResult = await buildingQuery(query)
-    if (queryResult.length === 0) {
-      interaction.reply({
-        content: `**Error:** The specified query, \`${query}\`, returned no building results.`,
-        ephemeral: true
-      })
-    } else if (queryResult.length === 1) {
+    const queryResult = await buildingQuery(query, true)
+    if (queryResult.length === 1) {
       interaction.reply({
         embeds: [buildingInfo(queryResult[0])]
       })
@@ -29,11 +24,8 @@ export default async function (interaction) {
       })
     }
   } catch (e) {
-    if (e instanceof BadGatewayError) {
-      interaction.reply({
-        content: '**Error:** The upstream server encountered an internal error when performing the requested search.',
-        ephemeral: true
-      })
+    if (e instanceof EmbeddableError) {
+      e.replyWithEmbed(interaction)
     }
   }
 }

discordBot/commands/ChatInput/logic/dining.js
@@ -1,3 +1,10 @@
+import { CommandUnavailableError } from '../../../../lib/errorBases.js'
+
+export default async function (interaction) {
+  new CommandUnavailableError().replyWithEmbed(interaction)
+}
+
+/*
 import _ from 'lodash'
 import { DateTime } from 'luxon'
 import { EmbedBuilder } from 'discord.js'
@@ -63,7 +70,6 @@ export default async function (interaction) {
         })
       }
     }
-  } /* else {
-    // Area for subcommand groups (unimplemented)
-  } */
+  }
 }
+*/

discordBot/commands/ChatInput/logic/localtime.js
@@ -1,4 +1,23 @@
 import { DateTime } from 'luxon'
+import { EmbeddableError } from '../../../../lib/errorBases.js'
+
+export class InvalidTimeZoneError extends EmbeddableError {
+  constructor (timeZone, { referenceNumber, cause } = {}) {
+    super(
+      {
+        errorMessage: `Time zone "${timeZone} is invalid.`,
+        embedMessage: `**The specified time zone, \`${timeZone}\`, is invalid.** Time zones must be specified in UTC offset format (e.g., \`UTC-05:00\`) or in [IANA/tz database format](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones) (e.g., \`America/New_York\`).`,
+        summaryMessage: 'Invalid time zone',
+        referenceNumber,
+        code: 'IB-U.6A5C748D'
+      },
+      {
+        cause
+      }
+    )
+    this.name = this.constructor.name
+  }
+}
 
 export default async function (interaction) {
   const timeZoneUnspecified = interaction.options.get('timezone') === null
@@ -13,14 +32,11 @@ export default async function (interaction) {
     } is:** ${formattedTime}\n` +
       `-# **Corresponding time in your current time zone:** <t:${time.toUnixInteger()}:f>\n` +
       `-# **Created:** <t:${time.toUnixInteger()}:R>`
-    interaction.reply({
+    return interaction.reply({
       content: formattedTime,
       ephemeral: interaction.guild !== null
     })
   } else {
-    interaction.reply({
-      content: `**Error:** The specified time zone, \`${timeZone}\`, is not valid.`,
-      ephemeral: true
-    })
+    return new InvalidTimeZoneError(timeZone).replyWithEmbed(interaction)
   }
 }

discordBot/commands/ChatInput/logic/resetnick.js
@@ -1,36 +1,50 @@
 import { authorizeServerAndReply } from '../../authorization.js'
 import { getAutoNickname } from '../../../../lib/userLinks.js'
+import { EmbeddableError } from '../../../../lib/errorBases.js'
+
+export class ResetNicknameError extends EmbeddableError {
+  constructor (userId, { referenceNumber, cause } = {}) {
+    super(
+      {
+        errorMessage: `Could not reset nickname for user ${userId}.`,
+        embedMessage: `**The nickname of user <@${userId}> could not be reset.** The most likely reason for this is that IdentiBot lacks the appropriate Discord permissions to reset this user's nickname.\nCheck to make sure that the user is not the server owner and does not have any roles that are higher in the role list than IdentiBot's highest role.`,
+        summaryMessage: 'Nickname reset failed',
+        referenceNumber,
+        code: 'IB-B.54B6E5CD'
+      },
+      {
+        cause
+      }
+    )
+    this.name = this.constructor.name
+  }
+}
 
 export default async function (interaction) {
   if (!await authorizeServerAndReply(interaction)) return
   const targetUser = interaction.options.get('user').user
-  const newNickname = await getAutoNickname(targetUser)
-  if (newNickname === false) {
-    interaction.reply({
-      content: `**Error:** User <@${targetUser.id}> has not linked a Kerberos account. Their nickname cannot be reset.`,
-      allowedMentions: {
-        parse: []
-      },
-      ephemeral: true
-    })
-    return
-  }
-  interaction.guild.members.fetch(targetUser.id).then((member) => {
-    return member.setNickname(newNickname, `Nickname reset requested by user ${interaction.user.id}.`)
-  }).then(
-    () => {
-      interaction.reply({
-        content: `Nickname of user <@${targetUser.id}> reset to ${newNickname}.`,
-        allowedMentions: {
-          parse: []
+  try {
+    const newNickname = await getAutoNickname(targetUser)
+    return interaction.guild.members.fetch(targetUser.id)
+      .then((member) => {
+        return member.setNickname(newNickname, `Nickname reset requested by user ${interaction.user.id}.`)
+      })
+      .then(
+        () => {
+          return interaction.reply({
+            content: `Nickname of user <@${targetUser.id}> reset to ${newNickname}.`,
+            allowedMentions: {
+              parse: []
+            },
+            ephemeral: true
+          })
         },
-        ephemeral: true
-      })
-    },
-    () => {
-      interaction.reply({
-        content: `**Error:** Nickname of user <@${targetUser.id}> could not be reset.`,
-        ephemeral: true
-      })
-    })
+        () => {
+          return new ResetNicknameError(targetUser.id).replyWithEmbed(interaction)
+        })
+  } catch (err) {
+    if (err instanceof EmbeddableError) {
+      return err.replyWithEmbed(interaction)
+    }
+  }
 }

discordBot/commands/ChatInput/logic/searchdir.js
@@ -1,24 +1,22 @@
 import * as webDirectory from '../../../../lib/publicAPIs/webDirectory.js'
 import { directoryResult, directoryResultList } from '../../../embedBuilders.js'
+import { EmbeddableError } from '../../../../lib/errorBases.js'
 
 export default async function (interaction) {
+  interaction.deferReply()
   try {
     const result = (await webDirectory.detailSearch(interaction.options.get('query').value))[0]
-    interaction.reply({
+    interaction.editReply({
       embeds: [await directoryResult(result)]
     })
   } catch (e) {
-    if (e instanceof webDirectory.NoDirectoryResultsError) {
-      interaction.reply({
-        content: `**Error:** The specified query, \`${interaction.options.get('query').value}\`, returned no directory results.`,
-        ephemeral: true
+    if (e instanceof webDirectory.MultipleDirectoryResultsError) {
+      return interaction.editReply({
+        embeds: [await directoryResultList(e.results, interaction.options.get('query').value)]
       })
     }
-    if (e instanceof webDirectory.MultipleDirectoryResultsError) {
-      interaction.reply({
-        embeds: [await directoryResultList(e.results, interaction.options.get('query').value)],
-        ephemeral: true
-      })
+    if (e instanceof EmbeddableError) {
+      return e.editReplyWithEmbed(interaction)
     }
   }
 }

discordBot/commands/ChatInput/logic/seeidphoto.js
@@ -1,8 +1,10 @@
 import { AttachmentBuilder, EmbedBuilder } from 'discord.js'
 import * as authorization from '../../authorization.js'
 import { readUserConfig } from '../../../../lib/configurationReaders.js'
-import { getKerberos, UnlinkedUserError } from '../../../../lib/userLinks.js'
+import { getKerberos } from '../../../../lib/userLinks.js'
 import { get as getPhoto } from '../../../../lib/mitDeveloperConnection/peoplePictures.js'
+import { EmbeddableError } from '../../../../lib/errorBases.js'
+import { UnauthorizedUserError } from '../../authorization.js'
 
 export default async function (interaction) {
   const targetUser = interaction.options.get('user').user
@@ -38,15 +40,11 @@ export default async function (interaction) {
         })
       })
       .catch(err => {
-        if (err instanceof UnlinkedUserError) {
-          return interaction.editReply({
-            content: '**Error:** This user does not have a linked Kerberos identity.'
-          })
+        if (err instanceof EmbeddableError) {
+          err.editReplyWithEmbed(interaction)
         }
       })
   } else {
-    return interaction.editReply({
-      content: '**Error:** You do not have permission to perform this action.\n-# The user may not have enabled member ID card photo access for their account, or they may have blocked you or this server.'
-    })
+    new UnauthorizedUserError(interaction.user.id).editReplyWithEmbed(interaction)
   }
 }

discordBot/commands/ChatInput/logic/usersettings.js
@@ -1,4 +1,5 @@
 import { readUserConfig, userConfigCollection } from '../../../../lib/configurationReaders.js'
+import { GuildOnlyCommandError } from '../../../../lib/errorBases.js'
 
 export default async function (interaction) {
   const isServerInteraction = interaction.guild !== null
@@ -74,10 +75,7 @@ export default async function (interaction) {
       case 'blockserver': {
         const targetServer = interaction.guild
         if (targetServer === null) {
-          return interaction.reply({
-            content: '**Error:** This command must be run in a server, not in a direct message.',
-            ephemeral: true
-          })
+          return new GuildOnlyCommandError().replyWithEmbed(interaction)
         }
         await userConfigCollection.updateOne({ _userId: interaction.user.id },
           {
@@ -120,10 +118,7 @@ export default async function (interaction) {
       case 'unblockserver': {
         const targetServer = interaction.guild
         if (targetServer === null) {
-          return interaction.reply({
-            content: '**Error:** This command must be run in a server, not in a direct message.',
-            ephemeral: true
-          })
+          return new GuildOnlyCommandError().replyWithEmbed(interaction)
         }
         await userConfigCollection.updateOne({ _userId: interaction.user.id },
           {

commit 44f81dbe8988132dde178c51220ecfdd53d0891e

Fixed bug introduced in d0e3180d
This commit fixes a bug with the /courseinfo command that would cause the bot to crash when an unknown course prefix was entered.

discordBot/commands/ChatInput/logic/courseinfo.js
@@ -22,7 +22,7 @@ export default async function (interaction) {
         embeds: [departmentInfo(idQuery, courses.departmentInformation[idQuery])]
       })
     } else {
-      return UnknownPrefixError(idQuery).replyWithEmbed(interaction)
+      return new UnknownPrefixError(idQuery).replyWithEmbed(interaction)
     }
   }
 }