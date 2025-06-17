ZelnickB/mit-identibot

commit 505440a7fb99d460e0abc5eacf424fccc120836c

Adapted userLinks.js for multiple verif. methods
This commit makes several changes to /lib/userLinks.js to prepare for support of multiple verification methods (for admitted students, alumni, etc.). Code throughout the repository has been updated to reflect the changes in this file.
This commit also optimizes imports in several files.
These changes have no impact on user experience.

discordBot/commands/common/whois.js
@@ -1,25 +1,25 @@
 import { AttachmentBuilder } from 'discord.js'
-
 import { getUserInfo } from '../../../lib/userLinks.js'
 import { whoisResult } from '../../embedBuilders.js'
-import { BadGatewayError, EmbeddableError } from '../../../lib/errorBases.js'
+import { EmbeddableError } from '../../../lib/errorBases.js'
+import { readUserConfig } from '../../../lib/configurationReaders.js'
+import { get as getIdPhoto } from '../../../lib/mitDeveloperConnection/peoplePictures.js'
 
 export async function respond (interaction) {
-  try {
-    const userInfo = await getUserInfo(interaction.options.get('user').user)
-    const files = []
-    if (userInfo.image !== undefined) {
-      files.push(new AttachmentBuilder(userInfo.image, { name: 'user.jpeg' }))
+  const userInfo = await getUserInfo(interaction.options.get('user').user)
+  const kerberosInfoAvailable = userInfo.kerberos !== undefined && !(userInfo.kerberos instanceof Error)
+  let embed
+  const files = []
+  if ('kerberos' in userInfo) {
+    if (userInfo.kerberos instanceof EmbeddableError) return userInfo.kerberos.editReplyWithEmbed(interaction)
+    const userConfig = await readUserConfig(interaction.user.id)
+    if (userConfig.allowIdPhotoLookup.moderator || userConfig.allowIdPhotoLookup.member) {
+      files.push(new AttachmentBuilder(await getIdPhoto(userInfo.kerberos.kerberosId), { name: 'user.jpeg' }))
     }
-    const embed = await whoisResult(interaction.options.get('user').user.id, userInfo)
-    interaction.editReply({
-      embeds: [embed],
-      files
-    })
-  } catch (e) {
-    if (e instanceof EmbeddableError) {
-      return e.editReplyWithEmbed(interaction)
-    }
-    new BadGatewayError('IdentiBot encountered a problem while fetching the information for this user.').editReplyWithEmbed(interaction)
+    embed = whoisResult(interaction.options.get('user').user.id, 'kerberos', userInfo.kerberos, files.length !== 0 ? 'attachment://user.jpeg' : undefined)
   }
+  return interaction.editReply({
+    embeds: [embed],
+    files
+  })
 }

discordBot/commands/ChatInput/logic/idcard.js
@@ -1,12 +1,15 @@
 import { AttachmentBuilder, EmbedBuilder } from 'discord.js'
-import { getUserInfo, UnlinkedUserError } from '../../../../lib/userLinks.js'
+import { EmbeddableError } from '../../../../lib/errorBases.js'
+import * as userLinks from '../../../../lib/userLinks.js'
+import { get as getIdPhoto } from '../../../../lib/mitDeveloperConnection/peoplePictures.js'
 
 export default async function (interaction) {
-  const showIdPhoto = interaction.options.getBoolean('withpicture') !== false
+  const showIdPhoto = interaction.options.get('withpicture') === null || interaction.options.getBoolean('withpicture')
   const ephemeral = interaction.options.getBoolean('preview') === true
-  interaction.deferReply({ ephemeral })
+  await interaction.deferReply({ ephemeral })
   try {
-    const userInfo = await getUserInfo(interaction.user, showIdPhoto)
+    const userInfo = (await userLinks.getUserInfo(interaction.user)).kerberos
+    if (userInfo === undefined) return new userLinks.MissingLinkError(interaction.user.id, 'kerberos', false).editReplyWithEmbed(interaction)
     const embedBuilder = new EmbedBuilder()
       .setAuthor({
         name: 'Massachusetts Institute of Technology'
@@ -20,7 +23,7 @@ export default async function (interaction) {
         },
         {
           name: 'Name',
-          value: userInfo.userInfo.displayName,
+          value: userInfo.displayName,
           inline: true
         }
       ])
@@ -28,8 +31,8 @@ export default async function (interaction) {
       .setFooter({
         text: 'Not valid for official identification purposes. Sent upon request.'
       })
-    if (userInfo.userInfo.affiliations.length > 0) {
-      const affiliationType = userInfo.userInfo.affiliations[0].type
+    if (userInfo.affiliations.length > 0) {
+      const affiliationType = userInfo.affiliations[0].type
       embedBuilder.addFields({
         name: 'Affiliation',
         value: affiliationType.charAt(0).toUpperCase() + affiliationType.substring(1),
@@ -38,26 +41,21 @@ export default async function (interaction) {
     }
     embedBuilder.addFields({
       name: 'Email/Kerberos',
-      value: `${userInfo.userInfo.email.replaceAll('_', '\\_')} (\`${userInfo.userInfo.kerberosId}\`)`,
+      value: `${userInfo.email.replaceAll('_', '\\_')} (\`${userInfo.kerberosId}\`)`,
       inline: true
     })
     const files = []
-    if (userInfo.image !== undefined) {
-      files.push(new AttachmentBuilder(userInfo.image, { name: 'user.jpeg' }))
+    if (showIdPhoto) {
+      files.push(new AttachmentBuilder(await getIdPhoto(userInfo.kerberosId), { name: 'user.jpeg' }))
       embedBuilder.setThumbnail('attachment://user.jpeg')
     }
-    return await interaction.editReply({
+    return interaction.editReply({
       embeds: [embedBuilder],
       files
     })
   } catch (e) {
-    if (e instanceof UnlinkedUserError) {
-      return await interaction.editReply({
-        content: `The user <@${interaction.user.id}> does not have a linked Kerberos identity.`,
-        allowedMentions: {
-          parse: []
-        }
-      })
+    if (e instanceof EmbeddableError) {
+      return e.editReplyWithEmbed(interaction)
     }
   }
 }

discordBot/commands/ChatInput/logic/seeidphoto.js
@@ -1,10 +1,9 @@
 import { AttachmentBuilder, EmbedBuilder } from 'discord.js'
 import * as authorization from '../../authorization.js'
 import { readUserConfig } from '../../../../lib/configurationReaders.js'
-import { getKerberos } from '../../../../lib/userLinks.js'
+import * as userLinks from '../../../../lib/userLinks.js'
 import { get as getPhoto } from '../../../../lib/mitDeveloperConnection/peoplePictures.js'
 import { EmbeddableError } from '../../../../lib/errorBases.js'
-import { UnauthorizedUserError } from '../../authorization.js'
 
 export default async function (interaction) {
   const targetUser = interaction.options.get('user').user
@@ -16,35 +15,40 @@ export default async function (interaction) {
   await interaction.deferReply({ ephemeral: true })
   const targetUserConfig = await readUserConfig(targetUser.id)
   if (targetUserConfig.allowIdPhotoLookup.member && !(targetUserConfig.blocked.users.includes(interaction.user.id) || targetUserConfig.blocked.servers.includes(interaction.guild.id))) {
-    return getKerberos(targetUser)
-      .then(kerb => getPhoto(kerb))
-      .then(photo => {
-        return interaction.editReply({
-          embeds: [
-            new EmbedBuilder()
-              .setTitle('MIT ID Card Photo')
-              .setColor(0x750014)
-              .setFields([
-                {
-                  name: 'Discord user',
-                  value: `<@${targetUser.id}> (\`${targetUser.id}\`)`
-                },
-                {
-                  name: 'Retrieved',
-                  value: `<t:${Math.round(Date.now() / 1000)}:f>`
-                }
-              ])
-              .setImage('attachment://user.jpeg')
-          ],
-          files: [new AttachmentBuilder(photo, { name: 'user.jpeg' })]
-        })
-      })
-      .catch(err => {
-        if (err instanceof EmbeddableError) {
-          err.editReplyWithEmbed(interaction)
+    return userLinks.getIdentifiers(targetUser)
+      .then(identifiers => {
+        if (!('kerberos' in identifiers)) return new userLinks.MissingLinkError(targetUser.id, 'kerberos', false).editReplyWithEmbed(interaction)
+        else {
+          return getPhoto(identifiers.kerberos)
+            .then(photo => {
+              return interaction.editReply({
+                embeds: [
+                  new EmbedBuilder()
+                    .setTitle('MIT ID Card Photo')
+                    .setColor(0x750014)
+                    .setFields([
+                      {
+                        name: 'Discord user',
+                        value: `<@${targetUser.id}> (\`${targetUser.id}\`)`
+                      },
+                      {
+                        name: 'Retrieved',
+                        value: `<t:${Math.round(Date.now() / 1000)}:f>`
+                      }
+                    ])
+                    .setImage('attachment://user.jpeg')
+                ],
+                files: [new AttachmentBuilder(photo, { name: 'user.jpeg' })]
+              })
+            })
+            .catch(err => {
+              if (err instanceof EmbeddableError) {
+                err.editReplyWithEmbed(interaction)
+              }
+            })
         }
       })
   } else {
-    new UnauthorizedUserError(interaction.user.id).editReplyWithEmbed(interaction)
+    new authorization.UnauthorizedUserError(interaction.user.id).editReplyWithEmbed(interaction)
   }
 }

discordBot/embedBuilders.js
@@ -4,6 +4,7 @@ import { boolToYesNo } from '../lib/utils.js'
 import { dbClient } from '../lib/mongoClient.js'
 import { createHash } from 'crypto'
 import getMarkdownFromName from '../lib/applicationEmojis.js'
+import { getStudentYearFromUserInfo } from '../lib/mitDeveloperConnection/people.js'
 
 const config = await configReader()
 const verificationLinksCollection = dbClient.collection('verification.links')
@@ -352,109 +353,113 @@ export async function directoryResult (detailSearchResult) {
   return embedBuilder
 }
 
-export async function whoisResult (discordID, userInfo) {
-  let affiliationColorCode
-  let affiliationEmoji = ' '
+export function whoisResult (discordID, infoSource, userInfo, thumbnailURL) {
+  let affiliationColorCode = 0xFFFFFF
+  let affiliationEmoji = ''
   const embedBuilder = new EmbedBuilder()
     .setTitle('Verified User Information')
     .setDescription(`The Discord user <@${discordID}> has been identified as the following individual.`)
-    .addFields(
-      {
-        name: 'Legal Name',
-        value: `${userInfo.userInfo.familyName}, ${userInfo.userInfo.givenName}${userInfo.userInfo.middleName ? ' ' + userInfo.userInfo.middleName : ''}`,
-        inline: true
-      },
-      {
-        name: 'Display Name',
-        value: userInfo.userInfo.displayName,
-        inline: true
-      },
-      {
-        name: 'Email/Kerberos',
-        value: `${userInfo.userInfo.email.replaceAll('_', '\\_')} (\`${userInfo.userInfo.kerberosId}\`)`
+  switch (infoSource) {
+    case 'kerberos': {
+      embedBuilder.addFields(
+        {
+          name: 'Legal Name',
+          value: `${userInfo.familyName}, ${userInfo.givenName}${userInfo.middleName ? ' ' + userInfo.middleName : ''}`,
+          inline: true
+        },
+        {
+          name: 'Display Name',
+          value: userInfo.displayName,
+          inline: true
+        },
+        {
+          name: 'Email/Kerberos',
+          value: `${userInfo.email.replaceAll('_', '\\_')} (\`${userInfo.kerberosId}\`)`
+        }
+      )
+      if (userInfo.phoneNumber) {
+        embedBuilder.addFields({
+          name: 'Phone Number',
+          value: userInfo.phoneNumber
+        })
       }
-    )
-  if (userInfo.userInfo.phoneNumber) {
-    embedBuilder.addFields({
-      name: 'Phone Number',
-      value: userInfo.userInfo.phoneNumber
-    })
-  }
-  let positionTitle, officeLocation
-  const departmentNames = []
-  let affiliationType
-  if (userInfo.userInfo.affiliations.length > 0) {
-    affiliationType = userInfo.userInfo.affiliations[0].type
-    if ('departments' in userInfo.userInfo.affiliations[0]) {
-      for (const department of userInfo.userInfo.affiliations[0].departments) {
-        departmentNames.push(department.name)
+      let positionTitle, officeLocation
+      const departmentNames = []
+      let affiliationType
+      if (userInfo.affiliations.length > 0) {
+        affiliationType = userInfo.affiliations[0].type
+        if ('departments' in userInfo.affiliations[0]) {
+          for (const department of userInfo.affiliations[0].departments) {
+            departmentNames.push(department.name)
+          }
+        }
+        positionTitle = userInfo.affiliations[0].title
+        officeLocation = userInfo.affiliations[0].office
+        switch (userInfo.affiliations[0].type) {
+          case 'faculty':
+            affiliationColorCode = 0x8800FF
+            affiliationEmoji = getMarkdownFromName('faculty', true)
+            break
+          case 'staff':
+            affiliationColorCode = 0x00CCFF
+            affiliationEmoji = getMarkdownFromName('staff', true)
+            break
+          case 'affiliate':
+            affiliationColorCode = 0xFFD900
+            affiliationEmoji = getMarkdownFromName('affiliate', true)
+            break
+          case 'student':
+            affiliationColorCode = 0x88FF00
+            affiliationEmoji = getMarkdownFromName('student', true)
+            break
+        }
+        embedBuilder.addFields({
+          name: 'Affiliation Type',
+          value: affiliationEmoji + affiliationType.charAt(0).toUpperCase() + affiliationType.substring(1),
+          inline: true
+        })
       }
+      if (positionTitle) {
+        embedBuilder.addFields({
+          name: 'Title',
+          value: positionTitle,
+          inline: true
+        })
+      }
+      if (departmentNames.length > 0) {
+        embedBuilder.addFields({
+          name: 'Departments',
+          value: departmentNames.join(', '),
+          inline: true
+        })
+      }
+      let classYear = getStudentYearFromUserInfo(userInfo)
+      if (classYear === 'G') {
+        classYear = 'Graduate Student'
+      } else if (classYear !== undefined) {
+        classYear = `Year ${classYear}`
+      }
+      if (classYear) {
+        embedBuilder.addFields({
+          name: 'Class Year',
+          value: classYear,
+          inline: true
+        })
+      }
+      if (officeLocation) {
+        embedBuilder.addFields({
+          name: 'Office',
+          value: officeLocation,
+          inline: true
+        })
+      }
+      embedBuilder.setFooter({
+        text: 'Information retrieved from the MIT People database.'
+      })
     }
-    positionTitle = userInfo.userInfo.affiliations[0].title
-    officeLocation = userInfo.userInfo.affiliations[0].office
-    switch (userInfo.userInfo.affiliations[0].type) {
-      case 'faculty':
-        affiliationColorCode = 0x8800FF
-        affiliationEmoji = getMarkdownFromName('faculty', true)
-        break
-      case 'staff':
-        affiliationColorCode = 0x00CCFF
-        affiliationEmoji = getMarkdownFromName('staff', true)
-        break
-      case 'affiliate':
-        affiliationColorCode = 0xFFD900
-        affiliationEmoji = getMarkdownFromName('affiliate', true)
-        break
-      case 'student':
-        affiliationColorCode = 0x88FF00
-        affiliationEmoji = getMarkdownFromName('student', true)
-        break
-      default:
-        affiliationColorCode = 0xFFFFFF
-        break
-    }
-    embedBuilder.addFields({
-      name: 'Affiliation Type',
-      value: affiliationEmoji + affiliationType.charAt(0).toUpperCase() + affiliationType.substring(1),
-      inline: true
-    })
-  } else affiliationColorCode = 0xFFFFFF
-  if (positionTitle) {
-    embedBuilder.addFields({
-      name: 'Title',
-      value: positionTitle,
-      inline: true
-    })
   }
-  if (departmentNames.length > 0) {
-    embedBuilder.addFields({
-      name: 'Departments',
-      value: departmentNames.join(', '),
-      inline: true
-    })
-  }
-  if (affiliationType === 'student') {
-    let classYear = userInfo.userInfo.affiliations[0].classYear
-    if (classYear === 'G') {
-      classYear = 'Graduate Student'
-    } else {
-      classYear = 'Year ' + classYear
-    }
-    embedBuilder.addFields({
-      name: 'Class Year',
-      value: classYear,
-      inline: true
-    })
-  }
-  if (officeLocation) {
-    embedBuilder.addFields({
-      name: 'Office',
-      value: officeLocation,
-      inline: true
-    })
-  }
-  if (userInfo.image !== undefined) {
-    embedBuilder.setThumbnail('attachment://user.jpeg')
+  if (thumbnailURL) {
+    embedBuilder.setThumbnail(thumbnailURL)
   }
   embedBuilder.setColor(affiliationColorCode)
   return embedBuilder

lib/guildUserModifier.js
@@ -2,26 +2,23 @@ import * as crypto from 'crypto'
 import * as timersPromises from 'timers/promises'
 import { DiscordAPIError } from 'discord.js'
 import * as configReaders from './configurationReaders.js'
-import * as peopleAPI from './mitDeveloperConnection/people.js'
 import { gateway } from './discordAPIClients.js'
-import { getKerberos } from './userLinks.js'
-import { UserNotFoundError } from './mitDeveloperConnection/people.js'
+import { getIdentifiers, getUserInfoFromIdentifiers } from './userLinks.js'
+import { getAffiliationFromUserInfo, getStudentYearFromUserInfo } from './mitDeveloperConnection/people.js'
 
 export async function getInformationForAssignment (accountSnowflake) {
-  const kerberos = await getKerberos({ id: accountSnowflake })
-  try {
-    const userInfo = await peopleAPI.getByKerberos(kerberos)
-    const affiliation = peopleAPI.getAffiliationFromUserInfo(userInfo)
-    const studentYear = peopleAPI.getStudentYearFromUserInfo(userInfo)
-    return {
-      kerberos,
-      affiliation,
-      studentYear
-    }
-  } catch (e) {
-    if (e instanceof UserNotFoundError) return { kerberos, affiliation: undefined, studentYear: undefined }
-    throw e
+  const userIdentifiers = await getIdentifiers({ id: accountSnowflake })
+  const information = {
+    kerberos: userIdentifiers.kerberos,
+    affiliation: undefined,
+    studentYear: undefined
   }
+  const userInfo = await getUserInfoFromIdentifiers(userIdentifiers)
+  if ('kerberos' in userInfo && !(userInfo.kerberos instanceof Error)) {
+    information.affiliation = getAffiliationFromUserInfo(userInfo.kerberos)
+    information.studentYear = getStudentYearFromUserInfo(userInfo.kerberos)
+  }
+  return information
 }
 
 export async function assignRolesFromIDs (serverSnowflake, accountSnowflake, info = undefined) {

lib/userLinks.js
@@ -1,7 +1,5 @@
 import { dbClient } from './mongoClient.js'
-import { getByKerberos } from './mitDeveloperConnection/people.js'
-import { readUserConfig } from './configurationReaders.js'
-import { get } from './mitDeveloperConnection/peoplePictures.js'
+import * as peopleAPI from './mitDeveloperConnection/people.js'
 import { parseNickname } from './utils.js'
 import { EmbeddableError } from './errorBases.js'
 
@@ -12,8 +10,8 @@ export class UnlinkedUserError extends EmbeddableError {
   constructor (userId, { referenceNumber, cause } = {}) {
     super(
       {
-        errorMessage: `Discord user ${userId} has not linked a Kerberos account.`,
-        embedMessage: `**The user <@${userId}> has not linked a Kerberos account.** Ask the user to verify themself, and then try again.`,
+        errorMessage: `Discord user ${userId} has not linked any accounts.`,
+        embedMessage: `**The user <@${userId}> has not linked any accounts.** Ask the user to verify themself, and then try again.`,
         summaryMessage: 'Unlinked user',
         referenceNumber,
         code: 'IB-U.95C1B42B'
@@ -26,43 +24,92 @@ export class UnlinkedUserError extends EmbeddableError {
   }
 }
 
-export async function getKerberos (user) {
-  return verificationLinksCollection.findOne(
-    {
-      discordAccountId: user.id
-    }
-  ).then((doc) => {
-    if (doc === null) {
-      return Promise.reject(new UnlinkedUserError(user.id))
-    } else {
-      return doc.petrock.sub
-    }
-  }).then((petrockSub) => {
-    return petrockUserInfoCacheCollection.findOne({
-      sub: petrockSub
-    })
-  }).then((cachedUserInfo) => {
-    return cachedUserInfo.email.replace(/@mit\.edu/gi, '')
-  })
+export class MissingLinkError extends EmbeddableError {
+  constructor (userId, missingLinkType, showMissingLinkTypeInEmbed = true, { referenceNumber, cause } = {}) {
+    super(
+      {
+        errorMessage: `Discord user ${userId} has not linked the required account type (${missingLinkType}) for this action.`,
+        embedMessage: `**The user <@${userId}> has not linked ${showMissingLinkTypeInEmbed ? `an account of type \`${missingLinkType}\`, which is required to perform this action.` : 'an account of the type required to perform this action.'}** Ask the user to verify themself using the proper account type, and then try again.`,
+        summaryMessage: 'Missing user link',
+        referenceNumber,
+        code: 'IB-U.501FD0BB'
+      },
+      {
+        cause
+      }
+    )
+    this.name = this.constructor.name
+  }
 }
 
-export async function getUserInfo (user, withProfilePicture) {
-  const kerberos = await getKerberos(user)
-  if (withProfilePicture === undefined) {
-    const userConfig = await readUserConfig(user.id)
-    withProfilePicture = userConfig.allowIdPhotoLookup.moderator || userConfig.allowIdPhotoLookup.member
+export async function getIdentifiers (user) {
+  const linksDocument = await verificationLinksCollection.findOne({
+    discordAccountId: user.id
+  })
+  if (linksDocument === null) throw new UnlinkedUserError(user.id)
+  const identifierNames = []
+  const promises = []
+  if ('petrock' in linksDocument) {
+    identifierNames.push('kerberos')
+    promises.push(
+      petrockUserInfoCacheCollection.findOne({
+        sub: linksDocument.petrock.sub
+      }).then((cachedUserInfo) => {
+        return cachedUserInfo.email.replace(/@mit\.edu/gi, '')
+      })
+    )
   }
-  const promises = [getByKerberos(kerberos)]
-  if (withProfilePicture) promises.push(get(kerberos))
-  return Promise.all(promises).then(([userInfo, image]) => ({ userInfo, image }))
+  // Code to get other identifiers
+  const identifiers = await Promise.all(promises)
+  return Object.fromEntries(identifierNames.map((val, idx) => [val, identifiers[idx]]))
+}
+
+export async function getUserInfoFromIdentifiers (identifiers) {
+  const identifierNames = []
+  const promises = []
+  for (const name in identifiers) {
+    identifierNames.push(name)
+    switch (name) {
+      case 'kerberos':
+        promises.push(peopleAPI.getByKerberos(identifiers.kerberos))
+      // Code to get user info based on other identifiers
+    }
+  }
+  const userInfo = flattenPromiseSettledResult(await Promise.allSettled(promises))
+  return Object.fromEntries(identifierNames.map((val, idx) => [val, userInfo[idx]]))
+}
+
+export function getPrioritizedUserInfo (userInfo) {
+  // Fetches the information that IdentiBot should "prioritize" when performing operations against this user.
+  // Order of priority: (1) Kerberos
+  let infoObject
+  if ('kerberos' in userInfo && !(userInfo.kerberos instanceof Error)) {
+    infoObject = userInfo.kerberos
+    infoObject.infoSource = 'kerberos'
+  } else throw new peopleAPI.UserNotFoundError()
+  return infoObject
+}
+
+export async function getUserInfo (user) {
+  return getUserInfoFromIdentifiers(await getIdentifiers(user))
 }
 
 export async function getAutoNickname (user) {
-  return getUserInfo(user, false).then(
-    (userInfo) => {
-      if (!userInfo) {
-        return false
-      }
-      return parseNickname(userInfo.userInfo.givenName, userInfo.userInfo.familyName)
-    })
+  return getUserInfo(user).then((userInfo) => {
+    if (userInfo.kerberos !== undefined && !(userInfo.kerberos instanceof Error)) {
+      return parseNickname(userInfo.kerberos.givenName, userInfo.kerberos.familyName)
+    }
+  })
+}
+
+function flattenPromiseSettledResult (settledResult) {
+  const resultArray = []
+  for (const i of settledResult) {
+    if (i.status === 'fulfilled') {
+      resultArray.push(i.value)
+    } else {
+      resultArray.push(i.reason)
+    }
+  }
+  return resultArray
 }

commit deaea20d6726ec75340f18cd077fa6d20ac85da5

Fixed bug introduced in 505440a7
This commit fixes a bug (introduced in 505440a7) that caused IdentiBot to determine whether a user consented to moderator ID photo access by reading the preferences of the user running /whois (rather than the target user).

discordBot/commands/common/whois.js
@@ -12,7 +12,7 @@ export async function respond (interaction) {
   const files = []
   if ('kerberos' in userInfo) {
     if (userInfo.kerberos instanceof EmbeddableError) return userInfo.kerberos.editReplyWithEmbed(interaction)
-    const userConfig = await readUserConfig(interaction.user.id)
+    const userConfig = await readUserConfig(interaction.options.get('user').user.id)
     if (userConfig.allowIdPhotoLookup.moderator || userConfig.allowIdPhotoLookup.member) {
       files.push(new AttachmentBuilder(await getIdPhoto(userInfo.kerberos.kerberosId), { name: 'user.jpeg' }))
     }