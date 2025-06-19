db63511b000ae1ad30a168374fcfe383b43597c3
Benjamin Zelnick <34197604+ZelnickB@users.noreply.github.com>
2025-06-19 12:53:03 -0400

    Fixed crash when using `/whois` on unlinked user
    This commit fixes a bug that caused IdentiBot to crash when `/whois` or “Get Profile from Linked Kerberos” was used on a user with no linked accounts.

--- discordBot/commands/common/whois.js
+++ discordBot/commands/common/whois.js
@@ -6,20 +6,26 @@ import { readUserConfig } from '../../../lib/configurationReaders.js'
 import { get as getIdPhoto } from '../../../lib/mitDeveloperConnection/peoplePictures.js'
 
 export async function respond (interaction) {
-  const userInfo = await getUserInfo(interaction.options.get('user').user)
-  const kerberosInfoAvailable = userInfo.kerberos !== undefined && !(userInfo.kerberos instanceof Error)
-  let embed
-  const files = []
-  if ('kerberos' in userInfo) {
-    if (userInfo.kerberos instanceof EmbeddableError) return userInfo.kerberos.editReplyWithEmbed(interaction)
-    const userConfig = await readUserConfig(interaction.options.get('user').user.id)
-    if (userConfig.allowIdPhotoLookup.moderator || userConfig.allowIdPhotoLookup.member) {
-      files.push(new AttachmentBuilder(await getIdPhoto(userInfo.kerberos.kerberosId), { name: 'user.jpeg' }))
+  try {
+    const userInfo = await getUserInfo(interaction.options.get('user').user)
+    const kerberosInfoAvailable = userInfo.kerberos !== undefined && !(userInfo.kerberos instanceof Error)
+    let embed
+    const files = []
+    if ('kerberos' in userInfo) {
+      if (userInfo.kerberos instanceof EmbeddableError) return userInfo.kerberos.editReplyWithEmbed(interaction)
+      const userConfig = await readUserConfig(interaction.options.get('user').user.id)
+      if (userConfig.allowIdPhotoLookup.moderator || userConfig.allowIdPhotoLookup.member) {
+        files.push(new AttachmentBuilder(await getIdPhoto(userInfo.kerberos.kerberosId), { name: 'user.jpeg' }))
+      }
+      embed = whoisResult(interaction.options.get('user').user.id, 'kerberos', userInfo.kerberos, files.length !== 0 ? 'attachment://user.jpeg' : undefined)
     }
-    embed = whoisResult(interaction.options.get('user').user.id, 'kerberos', userInfo.kerberos, files.length !== 0 ? 'attachment://user.jpeg' : undefined)
+    return interaction.editReply({
+      embeds: [embed],
+      files
+    })
+  } catch (err) {
+    if (err instanceof EmbeddableError) {
+      return err.editReplyWithEmbed(interaction)
+    } else throw err
   }
-  return interaction.editReply({
-    embeds: [embed],
-    files
-  })
 }
